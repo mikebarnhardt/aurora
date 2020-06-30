3e63c7da4ff085c71b52eb1c9141bc1d17ac6ac2
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2020-06-30 14:57:00 -0400

    Disable cssnano mergeLonghand.

--- css/postcss.config.js
+++ css/postcss.config.js
@@ -1,12 +1,21 @@
 let plugins = [
   require('postcss-import'),
   require('postcss-custom-media'),
-  require('postcss-css-variables')({ preserve: true }),
+  require('postcss-css-variables')({preserve: true}),
   require('autoprefixer')
 ];
 
 if ( process.env.NODE_ENV === 'production' ) {
-  plugins.push( require('cssnano') );
+  plugins.push(
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          mergeLonghand: false
+        }
+      ]
+    })
+  );
 }
 
 module.exports = {
