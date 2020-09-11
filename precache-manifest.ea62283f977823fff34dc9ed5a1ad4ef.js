c13ae67cd01bb7abe18518aacf58cc98ccadef65
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2020-09-11 13:41:56 -0400

    Updates

---
+++ precache-manifest.ea62283f977823fff34dc9ed5a1ad4ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab50aa336dfefd6e6d41cabf666f1c95",
+    "url": "/ancient/index.html"
+  },
+  {
+    "revision": "69f46ce32904ce37b3df",
+    "url": "/ancient/static/js/2.9bd2829d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ancient/static/js/2.9bd2829d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95ac7df6a43c150d6afc",
+    "url": "/ancient/static/js/main.fb8c7110.chunk.js"
+  },
+  {
+    "revision": "5488a9ea26961f4cd548",
+    "url": "/ancient/static/js/runtime-main.71f6db4e.js"
+  }
+]);
